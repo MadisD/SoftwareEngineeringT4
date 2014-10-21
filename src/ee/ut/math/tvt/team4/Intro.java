a3ea859db816c5b282387658f27498fae4f2f76c
MadisD <madisdreifeld@gmail.com>
2014-10-21 16:08:15 +0300

    jrun now functional, fixes #2

--- src/ee/ut/math/tvt/team4/Intro.java
+++ src/ee/ut/math/tvt/team4/Intro.java
@@ -7,10 +7,10 @@ public class Intro {
 	private static final Logger log = Logger.getLogger(Intro.class);
 	public static void main(String[] args) {
 
-		
 		IntroUI intro = new IntroUI();
 		log.info("New window has been opened, Should SHOW event time and name of the component (invocated class name) ");
 		intro.showInfo();
+		
 		
 		
 	}
