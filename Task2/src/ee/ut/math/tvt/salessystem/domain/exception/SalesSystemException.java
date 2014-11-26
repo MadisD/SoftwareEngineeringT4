f9da5fe5ab7634b329aa3d1cb67dc6737dd61297
pavelraav <pavelraav@gmail.com>
2014-11-26 20:26:28 +0200

    Task 2, refactoring
    :)

---
+++ Task2/src/ee/ut/math/tvt/salessystem/domain/exception/SalesSystemException.java
@@ -0,0 +1,9 @@
+package ee.ut.math.tvt.salessystem.domain.exception;
+
+public class SalesSystemException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
---
+++ Task2/src/ee/ut/math/tvt/salessystem/ui/model/SalesSystemTableModel.java
@@ -0,0 +1,83 @@
+package ee.ut.math.tvt.salessystem.ui.model;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import javax.swing.table.AbstractTableModel;
+
+import ee.ut.math.tvt.salessystem.domain.data.DisplayableItem;
+
+/**
+ * Generic table model implementation suitable for extending.
+ */
+public abstract class SalesSystemTableModel<T extends DisplayableItem> extends
+        AbstractTableModel {
+
+    private static final long serialVersionUID = 1L;
+
+    protected final String[] headers;
+
+    public SalesSystemTableModel(final String[] headers) {
+        this.headers = headers;
+ 
+    }
+
+    /**
+     * @param item
+     *            item describing selected row
+     * @param columnIndex
+     *            selected column index
+     * @return value displayed in column with specified index
+     */
+	protected abstract Object getColumnValue(T item, int columnIndex);
+
+    public int getColumnCount() {
+        return headers.length;
+    }
+
+    @Override
+    public String getColumnName(final int columnIndex) {
+        return headers[columnIndex];
+    }
+
+    public int getRowCount() {
+    	if (getRows() == null) {
+    		return 0;
+    	}
+        return getRows().size();
+    }
+
+    public Object getValueAt(final int rowIndex, final int columnIndex) {
+        return getColumnValue(getRows().get(rowIndex), columnIndex);
+    }
+
+    // search for item with the specified id
+    public T getItemById(final long id) {
+        for (final T item : getRows()) {
+            if (item.getId() == id)
+                return item;
+        }
+        throw new NoSuchElementException();
+    }
+
+    public List<T> getTableRows() {
+        return getRows();
+    }
+
+    public void populateWithData(final List<T> data) {
+    	getRows().clear();
+    	getRows().addAll(data);
+    }
+    
+    public void addRow(T row) {
+    	getRows().add(row);
+        fireTableDataChanged();
+    }
+    
+    public T getRow(int index) {
+        return getRows().get(index);
+    }
+    
+    public abstract List<T> getRows();
+    
+}
